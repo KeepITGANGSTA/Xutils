ebe161cec9eec0c434a997a0f1b37910a9cab496
KeepITGANGSTA <31384766+KeepITGANGSTA@users.noreply.github.com>
2017-08-29 20:58:53 +0800

    完成

---
+++ app/src/main/java/adapter/NewsAdapter.java
@@ -0,0 +1,10 @@
+package adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 李英杰 on 2017/8/29.
+ */
+
+public class NewsAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/bean/InfoBean.java
@@ -0,0 +1,8 @@
+package bean;
+
+/**
+ * Created by 李英杰 on 2017/8/29.
+ */
+
+public class InfoBean {
+}
